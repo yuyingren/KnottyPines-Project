dcfffcd44b3b842f280fc9aee1af1588af7bef5a
Yuying Ren <55150457+yuyingren@users.noreply.github.com>
2026-04-26 16:17:46 -0400

    Add files via upload

---
+++ pools/243/243.js
@@ -0,0 +1,17 @@
+window.CPPool243Init = function() { cp.model['243Data']={
+};cp.poolSlideResources['pool243']={
+};
+cp.model.data['243']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:true
+};
+cp.poolResources["243Images"]=[];
+cp.poolResources["243Videos"]=[
+];
+cp.poolResources["243SlideVideos"]=[
+];
+}
